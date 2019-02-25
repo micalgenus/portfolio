a8d6b8ce1645d1f19a56b89836817e24741bffbd
Micalgenus <micalgenus@gmail.com>
2019-02-25 20:54:04 +0900

    Create Layout Header

---
+++ src/Containers/Layout/Components/Header/Header.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class LayoutHeader extends Component {
+  render() {
+    return <div />;
+  }
+}
--- src/Containers/Layout/Layout.js
+++ src/Containers/Layout/Layout.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import { Header } from "./Components";
+
 import "./Layout.scss";
 
 export default class Layout extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <Header />
+      </div>
+    );
   }
 }
