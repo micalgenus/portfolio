d8db3e646719d65c348e28a51f230aa3db48c5cc
Micalgenus <micalgenus@gmail.com>
2019-02-26 16:15:51 +0900

    Apply code splitting
    ref: https://reactjs.org/docs/code-splitting.html#route-based-code-splitting

--- src/Containers/Layout/Layout.js
+++ src/Containers/Layout/Layout.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Suspense, Component } from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Header } from './Components';
 
 import './Layout.scss';
@@ -10,13 +10,15 @@ export default class Layout extends Component {
     return (
       <div>
         <Header />
-        <BrowserRouter>
-          <div>
-            {this.props.routers.map((route, idx) =>
-              route.Component ? <Route key={idx} exact={route.exact} path={route.path} component={route.Component} /> : null
-            )}
-          </div>
-        </BrowserRouter>
+        <Router>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              {this.props.routers.map((route, idx) =>
+                route.Component ? <Route key={idx} exact={route.exact} path={route.path} component={route.Component} /> : null
+              )}
+            </Switch>
+          </Suspense>
+        </Router>
       </div>
     );
   }
--- src/Router.js
+++ src/Router.js
@@ -1,4 +1,6 @@
-import Main from './Pages/Main';
-import Introduce from './Pages/Introduce';
+import { lazy } from 'react';
+
+const Main = lazy(() => import('./Pages/Main'));
+const Introduce = lazy(() => import('./Pages/Introduce'));
 
 export default [{ Component: Main, path: '/', exact: true }, { Component: Introduce, path: '/intro' }];
