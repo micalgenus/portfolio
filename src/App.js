ff96b9695fe6029724557c8a61902a169263b6f0
Micalgenus <micalgenus@gmail.com>
2019-02-26 15:34:43 +0900

    Create Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Layout from './Containers';
 
+import Router from './Router';
+
 class App extends Component {
   render() {
-    return <Layout />;
+    return <Layout {...this.props} routers={Router} />;
   }
 }
 
---
+++ src/Router.js
@@ -0,0 +1,4 @@
+import Main from './Pages/Main';
+import Introduce from './Pages/Introduce';
+
+export default [{ Component: Main, path: '/', exact: true }, { Component: Introduce, path: '/intro' }];
