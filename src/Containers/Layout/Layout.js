db0d4a90cd1623722b171e82d53970d1c821007d
Micalgenus <micalgenus@gmail.com>
2019-02-26 15:34:59 +0900

    Update Layout routers

--- src/Containers/Layout/Layout.js
+++ src/Containers/Layout/Layout.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Header } from './Components';
 
 import './Layout.scss';
@@ -8,7 +10,22 @@ export default class Layout extends Component {
     return (
       <div>
         <Header />
+        <BrowserRouter>
+          <div>
+            {this.props.routers.map((route, idx) =>
+              route.Component ? <Route key={idx} exact={route.exact} path={route.path} component={route.Component} /> : null
+            )}
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
 }
+
+Layout.defaultProps = {
+  routers: [],
+};
+
+Layout.propTypes = {
+  routers: PropTypes.array,
+};
