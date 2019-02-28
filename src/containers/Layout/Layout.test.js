2fc6d5cc7bfa32149872608bd3eb53acccca0479
Micalgenus <micalgenus@gmail.com>
2019-03-01 01:16:51 +0900

    Update Layout.test.js

--- src/containers/Layout/Layout.test.js
+++ src/containers/Layout/Layout.test.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import TestComponent from '@/TestComponent';
+import { shallow } from 'enzyme';
 import { expect } from 'chai';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 
 import Layout from './Layout';
 import Routers from '@/Router';
 
-Enzyme.configure({ adapter: new Adapter() });
-
-describe('/Containers/Layout', () => {
+const LayoutTest = new TestComponent({ name: '/Containers/Layout', component: Layout });
+LayoutTest.run(() => {
   it('Route render check', () => {
     const wrapper = shallow(<Layout routers={Routers} />);
     expect(wrapper.find(Route)).to.have.lengthOf(Routers.length);
