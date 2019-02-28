a96241035eda76dcbe2d64bc4d973d0fa06fcb40
Micalgenus <micalgenus@gmail.com>
2019-03-01 01:02:06 +0900

    Create Project page

--- src/Router.js
+++ src/Router.js
@@ -2,5 +2,13 @@ import { lazy } from 'react';
 
 const Main = lazy(() => import('@/Pages/Main'));
 const Introduce = lazy(() => import('@/Pages/Introduce'));
+const Projects = lazy(() => import('@/Pages/Projects'));
 
-export default [{ title: '홈', Component: Main, path: '/', exact: true }, { title: '소개', Component: Introduce, path: '/intro' }];
+const pathPrefix = process.env.CI === true ? '/portfolio' : '';
+const appendPrefix = path => pathPrefix + path;
+
+export default [
+  { title: '홈', Component: Main, path: '/', exact: true },
+  { title: '소개', Component: Introduce, path: '/intro' },
+  { title: '프로젝트', Component: Projects, path: '/projects' },
+].map(v => ({ ...v, path: appendPrefix(v.path) }));
