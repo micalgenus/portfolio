7c74602a0181dee7cf1a145629b45db57de19765
Micalgenus <micalgenus@gmail.com>
2019-03-01 00:02:34 +0900

    Update Header.test.js

--- src/containers/Layout/Components/Header/Header.test.js
+++ src/containers/Layout/Components/Header/Header.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import { expect } from 'chai';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { Link } from 'react-router-dom';
+
 import Header from './Header';
 import Routers from '@/Router';
 
-describe('Header', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Header />, div);
-    ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('/Containers/Layout/Components/Header', () => {
+  it('Link render check', () => {
+    const wrapper = shallow(<Header routers={Routers} />);
+    expect(wrapper.find(Link)).to.have.lengthOf(Routers.length);
   });
 });
