e9442e4dda48e21f337717e6da3d4816a94f4869
Micalgenus <micalgenus@gmail.com>
2019-03-01 01:16:54 +0900

    Update Header.test.js

--- src/containers/Layout/Components/Header/Header.test.js
+++ src/containers/Layout/Components/Header/Header.test.js
@@ -1,17 +1,15 @@
 import React from 'react';
-
-import { expect } from 'chai';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
 import { Link } from 'react-router-dom';
+
+import TestComponent from '@/TestComponent';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
 
 import Header from './Header';
 import Routers from '@/Router';
 
-Enzyme.configure({ adapter: new Adapter() });
-
-describe('/Containers/Layout/Components/Header', () => {
+const HeaderTest = new TestComponent({ name: '/Containers/Header', component: Header });
+HeaderTest.run(() => {
   it('Link render check', () => {
     const wrapper = shallow(<Header routers={Routers} />);
     expect(wrapper.find(Link)).to.have.lengthOf(Routers.length);
