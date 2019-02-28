6ebf4aa0d100546ca685b3fe0ae1f1105b42de82
Micalgenus <micalgenus@gmail.com>
2019-03-01 01:01:45 +0900

    Update App Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,5 @@
-import React from 'react';
-
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
+import TestComponent from '@/TestComponent';
 import App from './App';
 
-Enzyme.configure({ adapter: new Adapter() });
-
-describe('/App', () => {
-  it('snapshot test', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper).toMatchSnapshot();
-  });
-});
+const AppTest = new TestComponent({ name: '/App', component: App, snapshot: true });
+AppTest.run(() => {});
