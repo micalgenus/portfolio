2e412dc977126e57372286b9df1b9679d27bda07
Micalgenus <micalgenus@gmail.com>
2019-03-01 01:02:18 +0900

    Create Project test

---
+++ src/pages/Projects/Projects.test.js
@@ -0,0 +1,5 @@
+import TestComponent from '@/TestComponent';
+import Projects from './Projects';
+
+const ProjectsTest = new TestComponent({ name: '/Pages/Projects', component: Projects, snapshot: true });
+ProjectsTest.run(() => {});
