94b90f5ee7fbc8e7045b0bea2d0d24320637a3ad
Micalgenus <micalgenus@gmail.com>
2019-03-01 21:58:49 +0900

    Remove Introduce page

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import { lazy } from 'react';
 import { homepage } from 'package.json';
 
 const Main = lazy(() => import('@/Pages/Main'));
-const Introduce = lazy(() => import('@/Pages/Introduce'));
+// const Introduce = lazy(() => import('@/Pages/Introduce'));
 const Projects = lazy(() => import('@/Pages/Projects'));
 
 const parser = document.createElement('a');
@@ -13,6 +13,6 @@ const appendPrefix = path => pathPrefix + path;
 
 export default [
   { title: '홈', Component: Main, path: '/', exact: true },
-  { title: '소개', Component: Introduce, path: '/intro' },
+  // { title: '소개', Component: Introduce, path: '/intro' },
   { title: '프로젝트', Component: Projects, path: '/projects' },
 ].map(v => ({ ...v, path: appendPrefix(v.path) }));
