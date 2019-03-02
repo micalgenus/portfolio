e58bdbde18ed49907ef38f1ff37c8a7fee142c26
Micalgenus <micalgenus@gmail.com>
2019-03-03 03:56:53 +0900

    Update redux for browser height

--- src/store/modules/scroll.js
+++ src/store/modules/scroll.js
@@ -1,11 +1,13 @@
 // Action
 const CHANGE_SCROLL_Y = 'scroll/CHANGE_SCROLL_Y';
+const CHANGE_WINDOW_HEIGHT = 'scroll/CHANGE_WINDOW_HEIGHT';
 
 // Action Creator
 export const changeScrollY = scrollY => ({ type: CHANGE_SCROLL_Y, scrollY });
+export const changeWindowHeight = height => ({ type: CHANGE_WINDOW_HEIGHT, height });
 
 // Initial status
-const initialState = { scrollY: 0 };
+const initialState = { scrollY: 0, height: 0 };
 
 // Reducer
 export default function scroll(state = initialState, action) {
@@ -14,6 +16,11 @@ export default function scroll(state = initialState, action) {
       return {
         ...state,
         scrollY: action.scrollY,
+      };
+    case CHANGE_WINDOW_HEIGHT:
+      return {
+        ...state,
+        height: action.height,
       };
     default:
       return state;
