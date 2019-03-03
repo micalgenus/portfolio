df9fb2a541905e93396e0af842bdaf9d01cd3d1f
Micalgenus <micalgenus@gmail.com>
2019-03-03 16:27:52 +0900

    Apply redux-actions

--- src/store/modules/page.js
+++ src/store/modules/page.js
@@ -1,36 +1,34 @@
+import { createAction, handleActions } from 'redux-actions';
+
 // Action
 const CHANGE_PATH = 'page/CHANGE_PATH';
 const SHOW_LOADING = 'page/SHOW_LOADING';
 const HIDE_LOADING = 'page/HIDE_LOADING';
 
 // Action Creator
-export const changePath = path => ({ type: CHANGE_PATH, path });
-export const showLoading = (timeout = 5000) => ({ type: SHOW_LOADING, timeout }); // timeout => ms
-export const hideLoading = () => ({ type: HIDE_LOADING });
+export const changePath = createAction(CHANGE_PATH, path => path);
+export const showLoading = createAction(SHOW_LOADING, timeout => (timeout === 0 || timeout ? timeout : 5000));
+export const hideLoading = createAction(HIDE_LOADING);
 
 // Initial status
 const initialState = { path: '/', timeout: 5000, loading: false };
 
 // Reducer
-export default function page(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_PATH:
-      return {
-        ...state,
-        path: action.path,
-      };
-    case SHOW_LOADING:
-      return {
-        ...state,
-        timeout: action.timeout,
-        loading: true,
-      };
-    case HIDE_LOADING:
-      return {
-        ...state,
-        loading: false,
-      };
-    default:
-      return state;
-  }
-}
+export default handleActions(
+  {
+    [CHANGE_PATH]: (state, action) => ({
+      ...state,
+      path: action.payload,
+    }),
+    [SHOW_LOADING]: (state, action) => ({
+      ...state,
+      timeout: action.payload,
+      loading: true,
+    }),
+    [HIDE_LOADING]: (state, action) => ({
+      ...state,
+      loading: false,
+    }),
+  },
+  initialState
+);
--- src/store/modules/scroll.js
+++ src/store/modules/scroll.js
@@ -1,28 +1,27 @@
+import { createAction, handleActions } from 'redux-actions';
+
 // Action
 const CHANGE_SCROLL_Y = 'scroll/CHANGE_SCROLL_Y';
 const CHANGE_WINDOW_HEIGHT = 'scroll/CHANGE_WINDOW_HEIGHT';
 
 // Action Creator
-export const changeScrollY = scrollY => ({ type: CHANGE_SCROLL_Y, scrollY });
-export const changeWindowHeight = height => ({ type: CHANGE_WINDOW_HEIGHT, height });
+export const changeScrollY = createAction(CHANGE_SCROLL_Y, scrollY => scrollY);
+export const changeWindowHeight = createAction(CHANGE_WINDOW_HEIGHT, height => height);
 
 // Initial status
 const initialState = { scrollY: 0, height: 0 };
 
 // Reducer
-export default function scroll(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_SCROLL_Y:
-      return {
-        ...state,
-        scrollY: action.scrollY,
-      };
-    case CHANGE_WINDOW_HEIGHT:
-      return {
-        ...state,
-        height: action.height,
-      };
-    default:
-      return state;
-  }
-}
+export default handleActions(
+  {
+    [CHANGE_SCROLL_Y]: (state, action) => ({
+      ...state,
+      scrollY: action.payload,
+    }),
+    [CHANGE_WINDOW_HEIGHT]: (state, action) => ({
+      ...state,
+      height: action.payload,
+    }),
+  },
+  initialState
+);
