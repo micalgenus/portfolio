8c267795daea1553a65f81592023417fd6054527
Micalgenus <micalgenus@gmail.com>
2019-03-03 16:15:04 +0900

    Create Redux for Loading

--- src/store/modules/page.js
+++ src/store/modules/page.js
@@ -1,11 +1,15 @@
 // Action
 const CHANGE_PATH = 'page/CHANGE_PATH';
+const SHOW_LOADING = 'page/SHOW_LOADING';
+const HIDE_LOADING = 'page/HIDE_LOADING';
 
 // Action Creator
 export const changePath = path => ({ type: CHANGE_PATH, path });
+export const showLoading = (timeout = 5000) => ({ type: SHOW_LOADING, timeout }); // timeout => ms
+export const hideLoading = () => ({ type: HIDE_LOADING });
 
 // Initial status
-const initialState = { path: '/' };
+const initialState = { path: '/', timeout: 5000, loading: false };
 
 // Reducer
 export default function page(state = initialState, action) {
@@ -14,6 +18,17 @@ export default function page(state = initialState, action) {
       return {
         ...state,
         path: action.path,
+      };
+    case SHOW_LOADING:
+      return {
+        ...state,
+        timeout: action.timeout,
+        loading: true,
+      };
+    case HIDE_LOADING:
+      return {
+        ...state,
+        loading: false,
       };
     default:
       return state;
