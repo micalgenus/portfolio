367dddffe359862a246f7bcf94cb829ba4e51ed8
Micalgenus <micalgenus@gmail.com>
2019-03-03 16:07:38 +0900

    Change to functional component

--- src/components/ScrollPosition/index.js
+++ src/components/ScrollPosition/index.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './ScrollPosition.scss';
 
-export default class ScrollPosition extends Component {
-  render() {
-    const height = this.props.height >= 1 ? this.props.height : 1;
-    const scroll = this.props.scroll <= height ? (this.props.scroll > 0 ? this.props.scroll : 0) : this.props.height;
+const ScrollPosition = ({ top, scroll, height }) => {
+  const H = height >= 1 ? height : 1;
+  const S = scroll <= H ? (scroll > 0 ? scroll : 0) : height;
 
-    // document.getElementById('root').scrollHeight - document.documentElement.clientHeight
-    return (
-      <div className="scroll-component progress-container" id="progress-container" style={{ top: this.props.top }}>
-        <div className="progress-bar" id="page-status" style={{ width: `${(scroll / height) * 100}%` }} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="scroll-component progress-container" id="progress-container" style={{ top }}>
+      <div className="progress-bar" id="page-status" style={{ width: `${(S / H) * 100}%` }} />
+    </div>
+  );
+};
 
 ScrollPosition.defaultProps = {
   top: 0,
@@ -24,7 +21,9 @@ ScrollPosition.defaultProps = {
 };
 
 ScrollPosition.propTypes = {
-  top: PropTypes.number,
-  scroll: PropTypes.number,
-  height: PropTypes.number,
+  top: PropTypes.number.isRequired,
+  scroll: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
 };
+
+export default ScrollPosition;
