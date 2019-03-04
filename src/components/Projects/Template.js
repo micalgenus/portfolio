e23281042e19d8f7dcd0894eb53efb244006c4f0
Micalgenus <micalgenus@gmail.com>
2019-03-04 19:34:17 +0900

    Add Projects component CSS

--- src/components/Projects/Template.js
+++ src/components/Projects/Template.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import './Projects.scss';
+
 export default class ProjectTemplate extends Component {
   render() {
     return (
-      <div>
+      <div className="project-component">
         <h2>{this.props.title}</h2>
         <p>{this.props.description}</p>
         <div>{this.props.children}</div>
