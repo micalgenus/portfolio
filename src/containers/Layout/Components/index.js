b913650a61521100490f41badff09c268e086225
Micalgenus <micalgenus@gmail.com>
2019-03-04 21:37:44 +0900

    Move Layout Router Component

--- src/containers/Layout/Components/index.js
+++ src/containers/Layout/Components/index.js
@@ -1,6 +1,6 @@
 import Header from './Header';
 import Footer from './Footer';
-import Router from './Router';
+import Router from './RouterContainer';
 import ScrollTo from './ScrollTo';
 
 export { Header, Footer, Router, ScrollTo };
