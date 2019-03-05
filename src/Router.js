607797e27aa11d89bd09ab307367d936180c78bd
Micalgenus <micalgenus@gmail.com>
2019-03-05 20:58:48 +0900

    Remove route

--- src/Router.js
+++ src/Router.js
@@ -2,8 +2,8 @@ import { lazy } from 'react';
 import { homepage } from 'package.json';
 
 const Main = lazy(() => import('@/Pages/Main'));
-const Introduce = lazy(() => import('@/Pages/Introduce'));
-const Projects = lazy(() => import('@/Pages/Projects'));
+// const Introduce = lazy(() => import('@/Pages/Introduce'));
+// const Projects = lazy(() => import('@/Pages/Projects'));
 
 const parser = document.createElement('a');
 parser.href = homepage || 'http://localhost:3000';
@@ -13,6 +13,6 @@ export const appendPrefix = path => pathPrefix + path;
 
 export default [
   { title: '홈', Component: Main, path: '/', exact: true },
-  { title: '소개', Component: Introduce, path: '/intro' },
-  { title: '프로젝트', Component: Projects, path: '/projects' },
+  // { title: '소개', Component: Introduce, path: '/intro' },
+  // { title: '프로젝트', Component: Projects, path: '/projects' },
 ].map(v => ({ ...v, path: appendPrefix(v.path) }));
