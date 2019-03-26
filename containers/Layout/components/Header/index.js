5e0de21d5ff27a630481149cdc8117a7cb39c341
Micalgenus <micalgenus@gmail.com>
2019-03-26 18:20:37 +0900

    Remove duplicate code

--- containers/Layout/components/Header/index.js
+++ containers/Layout/components/Header/index.js
@@ -11,18 +11,6 @@ const HEADER_HEIGHT = 60;
 @inject('scroll')
 @observer
 class LayoutHeader extends Component {
-  renderScrollBar = () => {
-    const { top, scroll, height } = this.props;
-    const H = height >= 1 ? height : 1;
-    const S = scroll <= H ? (scroll > 0 ? scroll : 0) : height;
-
-    return (
-      <div className="scroll-component progress-container" id="progress-container" style={{ top }}>
-        <div className="progress-bar" id="page-status" style={{ width: `${(S / H) * 100}%` }} />
-      </div>
-    );
-  };
-
   render() {
     const { movedScrollY, scrollY, height } = this.props.scroll;
 
