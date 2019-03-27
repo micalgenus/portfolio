3695ed665d81a2e553af604cd66d8f2bbe2889f3
Micalgenus <micalgenus@gmail.com>
2019-03-27 19:31:01 +0900

    Remove console.log

--- stores/login.tsx
+++ stores/login.tsx
@@ -4,12 +4,10 @@ export class LoginStore {
   @observable loginPopup: boolean = false;
 
   @action showLoginPopup = () => {
-    console.log('showLoginPopup');
     this.loginPopup = true;
   };
 
   @action hideLoginPopup = () => {
-    console.log('hideLoginPopup');
     this.loginPopup = false;
   };
 }
