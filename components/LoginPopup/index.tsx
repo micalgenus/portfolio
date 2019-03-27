806a11df599f7995019822982ebbd17091b26c20
Micalgenus <micalgenus@gmail.com>
2019-03-27 19:31:15 +0900

    Remove code smell in LoginPopup

--- components/LoginPopup/index.tsx
+++ components/LoginPopup/index.tsx
@@ -13,52 +13,52 @@ export default class LoginPopup extends Component<StoreProps> {
     // do login
   };
 
+  renderLocalLogin = () => (
+    <div className="local-login">
+      <label>
+        <input placeholder=" " />
+        <span>Username</span>
+      </label>
+      <label>
+        <input placeholder=" " type="password" />
+        <span>Password</span>
+      </label>
+
+      <div className="account-helper">
+        <div className="remember-box">
+          <input type="checkbox" name="checkbox" value="value" />
+          <label>Remember Me</label>
+        </div>
+        <a>Forgot your password?</a>
+      </div>
+
+      <Button disabled fluid color="blue" className="login-button">
+        LOGIN
+      </Button>
+    </div>
+  );
+
+  renderSocialLogin = () => (
+    <div className="social-login">
+      <Button fluid color="blue">
+        Signup
+      </Button>
+      <Button icon labelPosition="left" fluid color="black">
+        <Icon size="large" inverted name="github" />
+        Github
+      </Button>
+    </div>
+  );
+
   render() {
     const { loginPopup, hideLoginPopup } = this.props.login || { loginPopup: false, hideLoginPopup: () => {} };
     return (
       <Modal closeIcon dimmer="blurring" size="mini" open={loginPopup} closeOnEscape={true} closeOnDimmerClick={false} onClose={hideLoginPopup}>
         <Modal.Content>
           <h3>Login</h3>
-          <div className="local-login">
-            <label>
-              <input placeholder=" " />
-              <span>Username</span>
-            </label>
-            <label>
-              <input placeholder=" " type="password" />
-              <span>Password</span>
-            </label>
-
-            <div className="account-helper">
-              <div className="remember-box">
-                <input type="checkbox" name="checkbox" value="value" />
-                <label>Remember Me</label>
-              </div>
-              <a>Forgot your password?</a>
-            </div>
-
-            <Button disabled fluid color="blue" className="login-button">
-              LOGIN
-            </Button>
-            {/* <div className="account-signup">
-              <a>Create new account?</a>
-            </div> */}
-          </div>
+          {this.renderLocalLogin()}
           <Divider horizontal>Or</Divider>
-
-          <div className="social-login">
-            <Button fluid color="blue">
-              Signup
-            </Button>
-            {/* <Button icon labelPosition="left" fluid color="red">
-              <Icon size="large" inverted name="google plus g" />
-              Google
-            </Button> */}
-            <Button icon labelPosition="left" fluid color="black">
-              <Icon size="large" inverted name="github" />
-              Github
-            </Button>
-          </div>
+          {this.renderSocialLogin()}
         </Modal.Content>
       </Modal>
     );
