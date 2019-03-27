364a1526cd89d7c2ea757be44c39a3d2300e73b5
Micalgenus <micalgenus@gmail.com>
2019-03-28 01:35:09 +0900

    Create swiper component

---
+++ components/Swiper/index.tsx
@@ -0,0 +1,57 @@
+import React, { Component, ReactElement } from 'react';
+
+import './Swiper.scss';
+
+interface State {
+  components: ReactElement[];
+  position: number;
+}
+
+export default class Swiper extends Component<{}, State> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = { components: this.makeChildrenList(props), position: 0 };
+  }
+
+  componentWillReceiveProps = (nextProps: any) => {
+    this.setState({ components: this.makeChildrenList(nextProps), position: 0 });
+  };
+
+  makeChildrenList = ({ children }: any): ReactElement[] => {
+    if (children) {
+      if (children.length) return children;
+      return [children];
+    }
+    return [];
+  };
+
+  next = () => {
+    console.log('next');
+    let position = this.state.position + 1;
+    if (position >= this.state.components.length) position = this.state.components.length - 1;
+    this.setState({ position });
+  };
+
+  pre = () => {
+    let position = this.state.position - 1;
+    if (position < 0) position = 0;
+    this.setState({ position });
+  };
+
+  render() {
+    const { components } = this.state;
+
+    return (
+      <div className="swiper-container">
+        <div className="swiper-content" style={{ width: `${100 * components.length}%`, left: `${-100 * this.state.position}%` }}>
+          {components.map((component, i) => (
+            <div key={i} className={i === this.state.position ? 'show-component' : 'hide-component'} style={{ width: `${100 / components.length}%` }}>
+              {component}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
--- components/index.tsx
+++ components/index.tsx
@@ -5,5 +5,6 @@ export { default as ExternalLinkIcon } from './ExternalLinkIcon';
 export { default as ItemTemplate } from './ItemTemplate';
 export { default as CategoryTemplate } from './CategoryTemplate';
 export { default as LoginPopup } from './LoginPopup';
+export { default as Swiper } from './Swiper';
 
 export { Link } from './Link';
