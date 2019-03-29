be35e75a5a30c6602b07d6d55368cffeb113207e
Micalgenus <micalgenus@gmail.com>
2019-03-30 01:49:54 +0900

    Remove code smell

--- containers/Layout/components/Header/index.tsx
+++ containers/Layout/components/Header/index.tsx
@@ -13,11 +13,16 @@ const HEADER_HEIGHT = 60;
 @inject('scroll')
 @observer
 class LayoutHeader extends Component<StoreProps> {
-  renderUserMenus = (username: string) => {
-    const { logout } = this.props.login || { logout: () => {} };
-    return (
+  renderUserMenus = () => {
+    const { logout, showLoginPopup, userInformation } = this.props.login || {
+      showLoginPopup: () => console.error('showLoginPopop Error'),
+      logout: () => console.error('logout Error'),
+      userInformation: { username: undefined },
+    };
+
+    return userInformation && userInformation.username ? (
       <li className="user-menu-items">
-        <Popup trigger={<span>{username}</span>} flowing hoverable hideOnScroll>
+        <Popup trigger={<span>{userInformation.username}</span>} flowing hoverable hideOnScroll>
           <Popup.Content>
             <ul>
               <li>
@@ -27,15 +32,15 @@ class LayoutHeader extends Component<StoreProps> {
           </Popup.Content>
         </Popup>
       </li>
+    ) : (
+      <li>
+        <a onClick={showLoginPopup}>Login</a>
+      </li>
     );
   };
 
   render() {
     const { movedScrollY, scrollY, height } = this.props.scroll || { movedScrollY: 0, scrollY: 0, height: 0 };
-    const { showLoginPopup, userInformation } = this.props.login || {
-      showLoginPopup: () => console.error('showLoginPopop Error'),
-      userInformation: { username: undefined },
-    };
 
     return (
       <header style={{ top: movedScrollY > 0 ? -HEADER_HEIGHT : 0 }}>
@@ -45,15 +50,7 @@ class LayoutHeader extends Component<StoreProps> {
               <a>Han GyeongSu's Portfolio</a>
             </Link>
           </div>
-          <ul>
-            {userInformation && userInformation.username ? (
-              this.renderUserMenus(userInformation.username)
-            ) : (
-              <li>
-                <a onClick={showLoginPopup}>Login</a>
-              </li>
-            )}
-          </ul>
+          <ul>{this.renderUserMenus()}</ul>
         </nav>
         <ScrollBar top={HEADER_HEIGHT} scroll={scrollY} height={height} />
       </header>
