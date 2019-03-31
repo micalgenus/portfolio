dc2f866b4a0ce7915a58332979280d171345ca91
Micalgenus <micalgenus@gmail.com>
2019-04-01 03:21:56 +0900

    Update retry in apollo client query
    retry = 5

--- lib/graphql/apollo.tsx
+++ lib/graphql/apollo.tsx
@@ -3,6 +3,7 @@ import { ApolloProvider } from 'react-apollo';
 
 import { ApolloClient } from 'apollo-client';
 import { ApolloLink, concat } from 'apollo-link';
+import { RetryLink } from 'apollo-link-retry';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import fetch from 'isomorphic-fetch';
@@ -13,6 +14,11 @@ import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
 const httpLink = createHttpLink({ uri: publicRuntimeConfig.PORTFOLIO_GRAPHQL_URL, fetch });
+const retryLink = new RetryLink({
+  delay: { initial: 300, max: Infinity, jitter: true },
+  attempts: { max: 5, retryIf: (error, _operation) => !!error },
+});
+
 const authMiddleware = new ApolloLink((operation, forward) => {
   operation.setContext({ headers: { 'x-access-token': getLoginToken() } });
 
@@ -21,7 +27,7 @@ const authMiddleware = new ApolloLink((operation, forward) => {
 });
 
 const client = new ApolloClient({
-  link: concat(authMiddleware, httpLink),
+  link: concat(authMiddleware, concat(retryLink, httpLink)),
   cache: new InMemoryCache(),
 });
 
