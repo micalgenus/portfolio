8dd5928a537f45e3a48cbc7f81d7105ef554675d
Micalgenus <micalgenus@gmail.com>
2019-04-01 00:49:32 +0900

    Update interface comment

--- interfaces/items.d.ts
+++ interfaces/items.d.ts
@@ -1,11 +1,22 @@
 import { SemanticICONS, SemanticCOLORS } from 'semantic-ui-react';
 
+/**
+ * @member icon
+ * @member color
+ * @member href
+ */
 export interface LinkItem {
   icon?: SemanticICONS;
   color?: SemanticCOLORS;
   href: string;
 }
 
+/**
+ * @member title
+ * @member links
+ * @member date
+ * @member description
+ */
 export interface DataItem {
   title: string;
   links: LinkItem[];
