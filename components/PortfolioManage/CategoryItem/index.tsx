70073a8b2510068b007543eaf7f7a3eef3a6d1da
Micalgenus <micalgenus@gmail.com>
2019-04-12 17:18:42 +0900

    Create updateCategoryItem

--- components/PortfolioManage/CategoryItem/index.tsx
+++ components/PortfolioManage/CategoryItem/index.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { ApolloClient } from 'apollo-client';
 
-import { Button, Icon } from 'semantic-ui-react';
+import { Icon } from 'semantic-ui-react';
 import { toast } from 'react-toastify';
+import { debounce } from 'lodash';
 
-import { removeCategoryItemQuery, getUserQuery } from '@/lib/graphql/query';
+import { InputText } from '@/components';
+import { removeCategoryItemQuery, getUserQuery, updateCategoryItemQuery } from '@/lib/graphql/query';
 
 import './CategoryItem.scss';
 import { Category, CategoryItem } from '@/interfaces';
@@ -16,20 +18,44 @@ interface Props {
   category: string;
 }
 
-export default class CategoryItemComponent extends Component<Props> {
+interface State {
+  name: string;
+}
+
+export default class CategoryItemComponent extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = { name: props.name };
+  }
+
+  onChangeText = (event: React.ChangeEvent<HTMLInputElement>, target: keyof State, callback: any) => {
+    this.setState({ [target]: event.target.value } as Pick<State, keyof State>);
+    callback();
+  };
+
+  updateItem = debounce(async () => {
+    const { client, _id, category } = this.props;
+    const { name } = this.state;
+    updateCategoryItem(client, category, { _id, name });
+  }, 1000);
+
   render() {
     const { _id, client, category, name } = this.props;
 
     return (
       <div className="category-item-manage-component" data-id={_id}>
+        <div className="remove-item-button" onClick={() => (confirm('Are you sure you want to delete?') ? removeCategoryItem(client, _id, category) : null)}>
+          <Icon name="trash" size="small" color="red" />
+        </div>
+
         <div className="category-item-handle">
           <Icon name="arrows alternate" />
         </div>
-        <h3>{name}</h3>
 
-        <Button color="red" onClick={() => removeCategoryItem(client, _id, category)}>
-          Remove
-        </Button>
+        <div className="category-item-manage-container">
+          <InputText label="Name" value={name} onChange={e => this.onChangeText(e, 'name', this.updateItem)} />
+        </div>
       </div>
     );
   }
@@ -51,5 +77,15 @@ function removeCategoryItem(client: ApolloClient<any>, id: string, category: str
       },
     })
     .then(() => toast.success('Completed remove item in user category.'))
+    .catch(err => toast.error(err.message));
+}
+
+function updateCategoryItem(client: ApolloClient<any>, category: string, { _id, name }: CategoryItem) {
+  return client
+    .mutate({
+      mutation: updateCategoryItemQuery,
+      variables: { id: _id, category, item: { name } },
+    })
+    .then(() => toast.success('Completed update category information of user.'))
     .catch(err => toast.error(err.message));
 }
--- lib/graphql/query.ts
+++ lib/graphql/query.ts
@@ -1,5 +1,8 @@
 import gql from 'graphql-tag';
 
+/**
+ * User information queries
+ */
 export const getUserQuery = gql`
   query {
     me {
@@ -43,12 +46,6 @@ export const getUserInfoQuery = gql`
   }
 `;
 
-export const createCategoryItemQuery = gql`
-  mutation createCategoryItem($category: String!) {
-    createCategoryItem(category: $category)
-  }
-`;
-
 export const updateUserInfoQuery = gql`
   mutation updateUserInfo($username: String, $email: String, $github: String, $linkedin: String, $description: String) {
     updateUserInfo(username: $username, email: $email, github: $github, linkedin: $linkedin, description: $description) {
@@ -61,15 +58,12 @@ export const updateUserInfoQuery = gql`
   }
 `;
 
+/**
+ * Category queries
+ */
 export const addCategoryQuery = gql`
   mutation {
     createCategory
-  }
-`;
-
-export const removeCategoryQuery = gql`
-  mutation removeCategory($id: String!) {
-    removeCategory(id: $id)
   }
 `;
 
@@ -88,14 +82,39 @@ export const updateCategorySequenceQuery = gql`
   }
 `;
 
-export const removeCategoryItemQuery = gql`
-  mutation removeCategoryItem($id: String!, $category: String!) {
-    removeCategoryItem(id: $id, category: $category)
+export const removeCategoryQuery = gql`
+  mutation removeCategory($id: String!) {
+    removeCategory(id: $id)
+  }
+`;
+
+/**
+ * Category item queries
+ */
+export const createCategoryItemQuery = gql`
+  mutation createCategoryItem($category: String!) {
+    createCategoryItem(category: $category)
+  }
+`;
+
+export const updateCategoryItemQuery = gql`
+  mutation updateCategoryItem($id: String!, $category: String!, $item: CategoryItemInput!) {
+    updateCategoryItem(id: $id, category: $category, item: $item) {
+      _id
+      name
+      description
+    }
   }
 `;
 
 export const updateCategoryItemSequenceQuery = gql`
   mutation updateCategoryItemSequence($category: String!, $sequences: [SequenceInput]!) {
     updateCategoryItemSequence(category: $category, sequences: $sequences)
+  }
+`;
+
+export const removeCategoryItemQuery = gql`
+  mutation removeCategoryItem($id: String!, $category: String!) {
+    removeCategoryItem(id: $id, category: $category)
   }
 `;
